a6da98d10358edc97a3814277ae07477400c95cb
MAC <mac@MacBook-Pro-MAC.local>
2026-06-22 13:05:37 +0400

    add project

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-01-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  typescript: {
+    strict: true,
+    typeCheck: false,
+  },
+})
